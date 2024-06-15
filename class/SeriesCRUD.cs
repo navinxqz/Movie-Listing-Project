74b0681e898799e0bbb5d3e140a9716de1b77443
navin-nawshin <navinmdnawshin@gmail.com>
2024-06-16 00:03:58 +0600

    CRUD methods added

--- class/SeriesCRUD.cs
+++ class/SeriesCRUD.cs
@@ -1,3 +1,34 @@
 namespace Project{
-    class SeriesCRUD : IRepo<Series>{}
+    public class SeriesCRUD : IRepo<SeriesMethods>{
+        private List<SeriesMethods> slist = new();
+        public List<SeriesMethods> List(){
+            return new List<SeriesMethods>(slist);
+            //return a new list to avoid external modification
+            }
+        public SeriesMethods GetId(int id){
+            return slist.Find(s => s.ID == id);
+            }
+        public void Add(SeriesMethods entity){
+            slist.Add(entity);
+        }
+        public void Delete(int id){
+            var series = GetId(id);
+            if(series != null){
+                series.Delete();
+                Console.WriteLine("Series deleted successfully.\n");
+            }else{
+                Console.WriteLine("Series not found!\n");
+            }
+        }
+        public void Update(int id, SeriesMethods entity){
+            int index = slist.FindIndex(s => s.ID ==id);
+            if(index != -1){
+                slist[index] = entity;
+                Console.WriteLine("Series updated successfully.\n");
+            }else{
+                Console.WriteLine("Series not found!\n");
+            }
+        }
+        public int NextId(){ return slist.Count+1; }
+    }
 }
